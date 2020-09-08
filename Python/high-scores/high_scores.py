4279f80316a49c5a114cc5472d5ef41a2fa28004
SauravJalui <sauravjalui.007@gmail.com>
2020-09-08 16:25:24 +0530

    updated high-scores

--- Python/high-scores/high_scores.py
+++ Python/high-scores/high_scores.py
@@ -1,10 +1,23 @@
-def latest(scores):
-    pass
+# class HighScores:
+# 	def __init__(self):
+# 		self.scores = scores
 
+# 	def latest(self):
+# 	    return self.scores[-1]
+
+# 	def personal_best(self):
+# 	    return max(self.scores)
+
+# 	def personal_top_three(self):
+# 	    return sorted(self.scores, reverse = True)[0:3]
+
+# to pass the tests had to use the below:
+scores = []
+def latest(scores):
+    return scores[-1]
 
 def personal_best(scores):
-    pass
-
+    return max(scores)
 
 def personal_top_three(scores):
-    pass
+    return sorted(scores, reverse = True)[0:3]
